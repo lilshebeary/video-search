0e1613ebf01e2562daff5065b20e45682a576f54
Ciri Ryan <cirijaye@yahoo.com>
2022-05-16 15:23:39 -0700

    add video and style in grid, add key

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,44 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
-import VideoList from './VideoList';
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
- class App extends React.Component {
+class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
-   const response = await youtube.get('/search', {
+    const response = await youtube.get("/search", {
       params: {
-        q: term
-      }
+        q: term,
+      },
     });
-      this.setState({ videos: response.data.items });
-  }
+    this.setState({ videos: response.data.items });
+  };
 
   onVideoSelect = (video) => {
-    console.log('from the App!', video);
-    
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
-      <div className='ui container' >
+      <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList 
-          videos={this.state.videos} 
-          onVideoSelect={this.onVideoSelect} 
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className='eleven wide column'>
+            <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    );
+    );  
   }
 }
 
