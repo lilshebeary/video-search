a691e06855bbc60632c65622d886e6a2c9dcff32
Ciri Ryan <cirijaye@yahoo.com>
2022-06-16 11:55:19 -0700

    refactor from class to function with hooks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,36 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
- class SearchBar extends React.Component {
-     state = { term: '' };
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
 
-     onInputChange = (e) => {
-        this.setState({ term: e.target.value });
-     };
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-     onFormSubmit = e => {
-         e.preventDefault();
+    onFormSubmit(term);
+  };
+  
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
-        this.props.onFormSubmit(this.state.term);
-     }
-
-
-  render() {
-    return (
-      <div className='search-bar ui segment'>
-          <form onSubmit={this.onFormSubmit} className='ui form'>
-              <div className='field'>
-                  <label>Video Search</label>
-                  <input 
-                    type='text' 
-                    value={this.state.term}
-                    onChange={this.onInputChange} 
-                    // onChange={e => this.setState({ term: e.target.value })}
-                />
-              </div>
-          </form>
-      </div>
-    )
-  }
-}
-
-export default SearchBar
+export default SearchBar;
